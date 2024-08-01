b498452f7064149a57a9d019f9e4f64caad059b3
Cameron Slash <caslash@sep.com>
2024-08-01 15:55:35 -0400

    Created tests

---
+++ TestTests/ViewModels/ContentViewModelTests.swift
@@ -0,0 +1,15 @@
+//
+//  ContentViewModelTests.swift
+//  TestTests
+//
+//  Created by Cameron S Slash on 8/1/24.
+//
+
+import Testing
+
+struct ContentViewModelTests {
+
+    @Test func canFetchPeople() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
