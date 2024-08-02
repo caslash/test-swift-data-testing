5ac9213545b3e09f23cf8f0f0faea560c7c500c5
Cameron Slash <calexslash@aol.com>
2024-08-01 23:51:28 -0400

    Update .gitignore

--- PeopleTests/Services/SwiftDataServiceTests.swift
+++ PeopleTests/Services/SwiftDataServiceTests.swift
@@ -9,7 +9,7 @@ import Testing
 
 struct SwiftDataServiceTests {
 
-    @Test func <#test function name#>() async throws {
+    @Test func canFetchPeople() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
 
